177b22d57c9b96bd960e49bc0715b4e9a08d0a88
chiman <chiman@sci.fi>
2011-05-03 18:29:05 +0300

    nice exit with ctrl-c

--- cursedsudoku.py
+++ cursedsudoku.py
@@ -12,11 +12,16 @@ if __name__ == '__main__':
                       help='input board file')
     options, args = parser.parse_args()
 
+    exit_message = ''
     cs = cursessudoku.CursesSudoku()
     try:
         if options.file is not None:
             cs.sudoku.load_from_file(options.file)
         cs.draw()
         cs.run()
+    except KeyboardInterrupt:
+        exit_message = 'User exit'
     finally:
         cs.quit()
+        if exit_message:
+            print exit_message
